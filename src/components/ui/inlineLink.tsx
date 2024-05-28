bef9d1c498cafcfe72f4571395030e2ff6c95c4d
Mateusz Siek <mateuszsiek91@gmail.com>
2024-05-28 14:26:30 +0200

    Layout: Header and footer

---
+++ src/components/ui/inlineLink.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface LinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+const InlineLink = React.forwardRef<HTMLInputElement, LinkProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <a
+        type={type}
+        className={cn("font-medium underline underline-offset-4", className)}
+        {...props}
+      />
+    );
+  }
+);
+InlineLink.displayName = "InlineLink";
+
+export { InlineLink };
