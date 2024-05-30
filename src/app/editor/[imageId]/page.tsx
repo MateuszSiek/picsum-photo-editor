cf32b0991e6ec99a1ed74f19795e0c69e4157b52
Mateusz Siek <mateuszsiek91@gmail.com>
2024-05-30 21:17:46 +0200

    Editor: Improved image loading

--- src/app/editor/[imageId]/page.tsx
+++ src/app/editor/[imageId]/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
+import { PicsumImage } from '@/lib/types';
 import { CanvasContextProvider } from './context';
 import { DesignPanel } from './designPanel';
 import ImagePreview from './imagePreview';
+import { useEffect, useState } from 'react';
+import { loadPicsumImage } from '@/lib/picsumApi';
 
 export default function Editor({ params }: { params: { imageId: string } }) {
+  const imageId = params.imageId;
+  const [image, setImage] = useState<PicsumImage>();
+
+  useEffect(() => {
+    loadPicsumImage(imageId).then(setImage);
+  }, [imageId]);
+
   return (
     <div className='grid h-full flex-1 items-stretch gap-6 md:grid-cols-[1fr_250px]'>
       <CanvasContextProvider>
-        <ImagePreview imageId={params.imageId} />
-        <DesignPanel imageId={params.imageId} />
+        <ImagePreview image={image} />
+        <DesignPanel image={image} />
       </CanvasContextProvider>
     </div>
   );
